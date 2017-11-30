fc880ce13143e92c63b883e0523f487c6eb63e96
Naren <developer.naren@gmail.com>
2017-11-30 11:16:29 +0545

    fixes

---
+++ examples/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+include '../vendor/autoload.php';
+
+$request = new \PV\RequestOut();
+
+$request->setTargetUrl('https://graph.facebook.com/343049269118318');//
+// will set params as eg: ?access_token=access_token
+//will set params as post
+
+$request->setParams(['access_token' => 'access_token']);
+$request->setHeader(['Authorization' => 'Bearer access_token']);//add header
+$request->setHeaders([
+    'header1' => 'value1',
+    'header2' => 'value2',
+]);//appends multiple headers
+
+$request->get();//for get request
+$request->post(); //for post request
+$request->postJson(); //to post data as json
---
+++ src/RequestOut.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace PV;
+
+use GuzzleHttp\Client;
+
+class RequestOut
+{
+    private $targetUrl;
+    private $params;
+    private $client;
+    private $requestType;
+    private $headers = [];
+    private $dataType = 'form_params';
+
+    public function __construct($targetUrl = null, $formData = [])
+    {
+        $this->client = new Client();
+        if (!empty($targetUrl)) {
+            $this->targetUrl = $targetUrl;
+        }
+
+        $this->params = $formData;
+    }
+
+    public function setHeader($header)
+    {
+        $this->headers = array_merge($this->headers, $header);
+    }
+
+    public function setHeaders(array $headers)
+    {
+        foreach ($headers as $header) {
+            $this->setHeader($header);
+        }
+    }
+
+    public function post()
+    {
+        $this->requestType = 'post';
+
+        return $this->request();
+    }
+
+    public function get()
+    {
+        $this->requestType = 'get';
+
+        return $this->request();
+    }
+
+    public function delete()
+    {
+        $this->requestType = 'delete';
+
+        return $this->request();
+    }
+
+    public function put()
+    {
+        $this->requestType = 'put';
+
+        return $this->request();
+    }
+
+    public function getTargetUrl()
+    {
+        return $this->targetUrl;
+    }
+
+    public function setTargetUrl($targetUrl)
+    {
+        return $this->targetUrl = $targetUrl;
+    }
+
+    public function setParams($params)
+    {
+        return $this->params = $params;
+    }
+
+    public function postJson()
+    {
+        $this->requestType = 'post';
+        $this->dataType = 'json';
+
+        return $this->request();
+    }
+
+    private function request()
+    {
+        $requestType = $this->requestType;
+
+        $request = $this->getRequestInstance($requestType);
+
+        return $request->getBody()->getContents();
+    }
+
+    /**
+     * @param $requestType
+     *
+     * @return mixed
+     */
+    private function getRequestInstance($requestType)
+    {
+        if (in_array($requestType, ['get', 'delete'])) {
+            if (!empty($this->params)) {
+                $this->targetUrl .= '?'.http_build_query($this->params);
+            }
+
+            if (!empty($this->headers)) {
+                $this->params['headers'] = $this->headers;
+
+                return $this->client->$requestType($this->targetUrl, ['headers' => $this->headers]);
+            }
+
+            return $this->client->$requestType($this->targetUrl);
+        }
+
+        $this->params = [$this->dataType => $this->params];
+
+        if (!empty($this->headers)) {
+            $this->params['headers'] = $this->headers;
+        }
+
+        return $this->client->$requestType($this->targetUrl, $this->params);
+    }
+}
